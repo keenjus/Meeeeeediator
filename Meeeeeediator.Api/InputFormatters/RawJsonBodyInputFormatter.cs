9675322196e2a6a116c9d170239ac3375c185c8c
Martin Petoffer <martinpetoffer@gmail.com>
2019-09-05 20:46:49 +0300

    Small refactoring with resharper

--- Meeeeeediator.Api/InputFormatters/RawJsonBodyInputFormatter.cs
+++ Meeeeeediator.Api/InputFormatters/RawJsonBodyInputFormatter.cs
@@ -15,11 +15,9 @@ namespace Meeeeeediator.Api.InputFormatters
         public override async Task<InputFormatterResult> ReadRequestBodyAsync(InputFormatterContext context)
         {
             var request = context.HttpContext.Request;
-            using (var reader = new StreamReader(request.Body))
-            {
-                var content = await reader.ReadToEndAsync();
-                return await InputFormatterResult.SuccessAsync(content);
-            }
+            using var reader = new StreamReader(request.Body);
+            string content = await reader.ReadToEndAsync();
+            return await InputFormatterResult.SuccessAsync(content);
         }
 
         protected override bool CanReadType(Type type)
