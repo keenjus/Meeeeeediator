ddd7587a7ec824c508706100ae56642bef1bee7a
Martin Petoffer <martinpetoffer@gmail.com>
2019-11-10 19:06:11 +0200

    Add blazor project and docker

---
+++ Meeeeeediator.Blazor/Startup.cs
@@ -0,0 +1,56 @@
+using Meeeeeediator.Core;
+using Meeeeeediator.Core.Interfaces;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Net.Http;
+
+namespace Meeeeeediator.Blazor
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRazorPages();
+            services.AddServerSideBlazor();
+
+            services.AddHttpClient("Mediator", client => client.BaseAddress = new Uri(Configuration["MediatorApi"]));
+            services.AddScoped<IMediator, ProxyMediator>(sp => new ProxyMediator(sp.GetRequiredService<IHttpClientFactory>().CreateClient("Mediator")));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapBlazorHub();
+                endpoints.MapFallbackToPage("/_Host");
+            });
+        }
+    }
+}
